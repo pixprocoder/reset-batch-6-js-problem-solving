1bdffae7a1df4dcc084d2652aec7792832fa5ad2
skpixels <makkobir79@gmail.com>
2023-03-11 16:26:11 +0600

    problem 1 solved

--- problem-solveing.js
+++ problem-solveing.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+// Problem 1: 1 থেকে n পর্যন্ত সংখ্যার যোগফল নির্ণয় করো।
+let n = 10;
+let sum = 0; // If we want to addition anything and store them, the initial value must 0;
+for (let i = 1; i <= n; i++) {
+  sum = sum + i;
+}
+console.log(sum);
