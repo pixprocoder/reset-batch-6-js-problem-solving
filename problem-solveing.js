5741b5c568e027d78b8f745d31725d9578b5cda7
skpixels <makkobir79@gmail.com>
2023-03-11 16:38:21 +0600

    problem 2 solved

--- problem-solveing.js
+++ problem-solveing.js
@@ -4,4 +4,24 @@ let sum = 0; // If we want to addition anything and store them, the initial valu
 for (let i = 1; i <= n; i++) {
   sum = sum + i;
 }
-console.log(sum);
+// console.log(sum);
+
+// Problem 2: ৩ এর নামতা লিখো জাভাস্ক্রিপ্ট ব্যাবহার করে।
+/*
+  3 * 1=3
+  3 * 2=6
+  3 * 3=9
+  3 * 4=12
+  3 * 5=15
+  3 * 6=18
+  3 * 7=21
+  3 * 8=24
+  3 * 9=27
+  3 *10=30
+ */
+
+let sum2 = 1; // If operation is multiply the initial value must >- 1
+for (let i = 1; i <= 10; i++) {
+  sum2 = i * 5;
+  //   console.log(sum2);
+}
